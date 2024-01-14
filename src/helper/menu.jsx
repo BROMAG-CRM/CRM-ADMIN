cabb6c0f0416515ef33d77ff4d4affa9cc1a27db
Abdul Muhaim <muhaimbromag@gmail.com>
2024-01-14 12:11:17 +0530

    support

--- src/helper/menu.jsx
+++ src/helper/menu.jsx
@@ -32,11 +32,11 @@ export const adminItems = [
     getItem(<NavLink to='bdm' className="text-[10px] lg:text-[14px]">BDM</NavLink>, '/bdm', <AddBusinessIcon className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='legalmanagement' className="text-[10px] lg:text-[14px]">Legal Management</NavLink>, '/legalmanagement', <PolicyIcon  className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='accountsmanagement' className="text-[10px] lg:text-[14px]">Accounts Management</NavLink>, '/accountmanagemant', <ManageAccountsIcon  className='!text-[17px] !text-[#CD5C08]'/>),
-    getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
-      getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
-      getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
-      getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
-    ]),
+    // getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
+    //   getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
+    //   getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
+    //   getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
+    // ]),
   ];
 
 
@@ -45,11 +45,11 @@ export const adminItems = [
     getItem(<NavLink to='/' className="text-[10px] lg:text-[14px]">Dashboard</NavLink>, '/', <DashboardCustomizeIcon className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='leadgeneration' className="text-[10px] lg:text-[14px]">Lead Generation</NavLink>, '/leadgeneration', <AccountCircleIcon  className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='telemarketing' className="text-[10px] lg:text-[14px]">Telemarketing</NavLink>, '/telemarketing', <AddIcCallIcon className='!text-[17px] !text-[#CD5C08]'/>),
-    getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
-      getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
-      getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
-      getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
-    ]),
+    // getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
+    //   getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
+    //   getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
+    //   getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
+    // ]),
   ];
 
 
@@ -58,11 +58,11 @@ export const adminItems = [
     getItem(<NavLink to='/' className="text-[10px] lg:text-[14px]">Dashboard</NavLink>, '/', <DashboardCustomizeIcon className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='leadgeneration' className="text-[10px] lg:text-[14px]">Lead Generation</NavLink>, '/leadgeneration', <AccountCircleIcon  className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='telesales' className="text-[10px] lg:text-[14px]">Telesales</NavLink>, '/telesales', <WifiCallingIcon  className='!text-[17px] !text-[#CD5C08]'/>),
-    getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
-      getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
-      getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
-      getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
-    ]),
+    // getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
+    //   getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
+    //   getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
+    //   getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
+    // ]),
   ];
 
 
@@ -71,11 +71,11 @@ export const adminItems = [
     getItem(<NavLink to='/' className="text-[10px] lg:text-[14px]">Dashboard</NavLink>, '/', <DashboardCustomizeIcon className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='leadgeneration' className="text-[10px] lg:text-[14px]">Lead Generation</NavLink>, '/leadgeneration', <AccountCircleIcon  className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='bdm' className="text-[10px] lg:text-[14px]">BDM</NavLink>, '/bdm', <AddBusinessIcon className='!text-[17px] !text-[#CD5C08]'/>),
-    getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
-      getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
-      getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
-      getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
-    ]),
+    // getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
+    //   getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
+    //   getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
+    //   getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
+    // ]),
   ];
 
 
@@ -84,11 +84,11 @@ export const adminItems = [
     getItem(<NavLink to='/' className="text-[10px] lg:text-[14px]">Dashboard</NavLink>, '/', <DashboardCustomizeIcon className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='leadgeneration' className="text-[10px] lg:text-[14px]">Lead Generation</NavLink>, '/leadgeneration', <AccountCircleIcon  className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='legalmanagement' className="text-[10px] lg:text-[14px]">Legal Management</NavLink>, '/legalmanagement', <PolicyIcon  className='!text-[17px] !text-[#CD5C08]'/>),
-    getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
-      getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
-      getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
-      getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
-    ]),
+    // getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
+    //   getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
+    //   getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
+    //   getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
+    // ]),
   ];
 
 
@@ -97,9 +97,9 @@ export const adminItems = [
     getItem(<NavLink to='/' className="text-[10px] lg:text-[14px]">Dashboard</NavLink>, '/', <DashboardCustomizeIcon className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='leadgeneration' className="text-[10px] lg:text-[14px]">Lead Generation</NavLink>, '/leadgeneration', <AccountCircleIcon  className='!text-[17px] !text-[#CD5C08]'/>),
     getItem(<NavLink to='accountsmanagement' className="text-[10px] lg:text-[14px]">Accounts Management</NavLink>, '/accountmanagemant', <ManageAccountsIcon  className='!text-[17px] !text-[#CD5C08]'/>),
-    getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
-      getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
-      getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
-      getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
-    ]),
+    // getItem(<div className='text-[10px] lg:text-[14px]'>Support</div>, 'sub3', <SupportAgentIcon className='!text-[17px] !text-[#CD5C08]' />, [
+    //   getItem(<a href="tel:+9150289762" className="text-[10px] lg:text-[14px]">Call</a>, '/call'),
+    //   getItem(<a href="https://wa.me/9150289762" className="text-[10px] lg:text-[14px]">Whatsapp</a>, '/whatsapp'),
+    //   getItem(<a href="mailto:example@example.com" className="text-[10px] lg:text-[14px]">Email</a>, '/email'),
+    // ]),
   ];
